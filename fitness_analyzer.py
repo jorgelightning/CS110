a74138a3a24c042d5cb89c6c792236bacd0b5f66
JP <2220635+jorgelightning@users.noreply.github.com>
2023-10-06 02:35:27 -0700

    Add files via upload

--- fitness_analyzer.py
+++ fitness_analyzer.py
@@ -1,27 +1,68 @@
 #Lab 4 - Files and Functions Part 2
 
 def main():
-	step_total = 0
-	hr_total = 0
-	sleep_total = 0
-	count = 0
-	fileOpen = open('fitness-data.txt',"r")
+	infile = open('fitness-data.txt',"r")
+	outfile = open('fitness-date-result.txt', 'w')
+	steps_total, steps_max = steps_analyzer(infile)
+	print('The total number of steps taken this week were:',steps_total)
+	print('The maximum number of steps taken this week were:',steps_max)
+	heart_rate_average = heart_rate_analyzer(infile)
+	print('The average heart rate for the week was:',"%.2f" % heart_rate_average)
+	sleep_total, sleep_min = sleep_analyzer(infile)
+	print('The total number of hours slept this week was:',sleep_total)
+	print('The least amount of hours slept this week was:',sleep_min)
+	write_results_file(outfile,steps_total,steps_max,heart_rate_average,sleep_total,sleep_min)
 
-	steps = range(1,22)
-	for n in steps:
-		line = fileOpen.readline()
+def write_results_file(outfile,steps_total,steps_max,heart_rate_average,sleep_total,sleep_min):
+	wsteps_total = str(f'The total number of steps taken this week were: {steps_total} .\n')
+	wsteps_max = str(f'The maximum number of steps taken this week were: {steps_max} .\n')
+	wheart_rate_average = str(f'The average heart rate for the week was: {"%.2f" % heart_rate_average} .\n')
+	wsleep_total = str(f'The total number of hours slept this week was: {sleep_total} .\n')
+	wsleep_min = str(f'The least amount of hours slept this week was: {sleep_min} .\n')
+	outfile.write(wsteps_total)
+	outfile.write(wsteps_max)
+	outfile.write(wheart_rate_average)
+	outfile.write(wsleep_total)
+	outfile.write(wsleep_min)
+
+def steps_analyzer(infile):
+	steps_total = 0
+	line_number = 1
+	step_list = []
+	for line in infile:
 		line = line.strip()
-		if(n <= 7):
-			step_total += int(line) 
-			print('1-7',line)
-		if(n >= 8 and n <= 14):
-			hr_total += int(line)
-			print('8-14',line)
-		if(n >= 15):
-			sleep_total += float(line) 
-			print('15 over',line)
-	print('Steps:',step_total)
-	print('Heart Rate:',hr_total)
-	print('Sleeps:',sleep_total)	
+		if(line_number <= 7):
+				steps_total += int(line)
+				step_list.append(int(line))
+		line_number += 1
+	steps_max = max(step_list)
+	return steps_total, steps_max
 
-main() 
+
+def heart_rate_analyzer(infile):
+	infile.seek(0) #This is used to reset the read file position 
+	heart_rate_average = 0
+	line_number = 1
+	for line in infile:
+		line = line.strip()
+		if(line_number >= 8 and line_number <= 14):
+				heart_rate_average += int(line)/7
+		line_number += 1
+	return heart_rate_average
+
+def sleep_analyzer(infile):
+	infile.seek(0) #This is used to reset the read file position 
+	sleep_total = 0
+	line_number = 1
+	sleep_list = []
+	for line in infile:
+		line = line.strip()
+		if(line_number >= 15):
+				sleep_total += float(line)
+				sleep_list.append(float(line))
+		line_number += 1
+	sleep_min = min(sleep_list)
+	return sleep_total,sleep_min
+
+
+main()
