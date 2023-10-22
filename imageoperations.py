63f34165800359991b7ac2bca654eba8e68c9c14
JP <2220635+jorgelightning@users.noreply.github.com>
2023-10-22 01:04:13 -0700

    Add files via upload

--- imageoperations.py
+++ imageoperations.py
@@ -13,6 +13,10 @@ def main():
     if(userAction == 1):
         imageFlipper(inputImage, imageWidth, imageHeight)
 
+    if(userAction == 2):
+        imageGrayScale(inputImage)
+
+
 
 # Creates a copy of an image given the image variable, its width, and height
 def copyImage(inputImage, imageWidth, imageHeight):
@@ -35,6 +39,18 @@ def imageFlipper(inputImage, imageWidth, imageHeight):
             imageFlipped.putpixel((x, y), inputImage.getpixel((imageWidth - x - 1, y)))
 
     imageFlipped.save("/Users/notion/Desktop/CS110/project2/flipped_image.png")
+
+def imageGrayScale(inputImage):
+
+    imagePixel = inputImage.getdata()
+
+    grayscalePixelData = []
+    for pixel in imagePixel:
+        grayscalePixel = (pixel[0] + pixel[1] + pixel[2]) // 3
+        grayscalePixelData.append((grayscalePixel,grayscalePixel,grayscalePixel))
+    
+    inputImage.putdata(grayscalePixelData)
+    inputImage.save("/Users/notion/Desktop/CS110/project2/grayscale_image.png")
 
 
 main()
