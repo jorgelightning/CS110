a6acb6a509ab7b9e4502b04741c6fcdca7599e97
JP <2220635+jorgelightning@users.noreply.github.com>
2023-10-22 00:44:58 -0700

    Add files via upload

---
+++ imageoperations.py
@@ -0,0 +1,40 @@
+from PIL import Image
+
+def main():
+ #   imageName = input('What image should you like to take action on (enter image name and exention): ')
+    imageName = 'usfca_logo.png'
+    inputImage = Image.open('/Users/notion/Desktop/CS110/project2/'+imageName)
+    userAction = int(input('\nWelcome to image Operation program!\n\nWhat action would you like to take?\n(0)copy image (1)flip image (2)find pattern (3)make grey scale\n'))
+    imageWidth, imageHeight = inputImage.size
+
+    if(userAction == 0):
+        copyImage(inputImage, imageWidth, imageHeight)
+
+    if(userAction == 1):
+        imageFlipper(inputImage, imageWidth, imageHeight)
+
+
+# Creates a copy of an image given the image variable, its width, and height
+def copyImage(inputImage, imageWidth, imageHeight):
+    copyImageOutput = Image.new('RGB', (imageWidth, imageHeight), 'white')
+
+    for i in range(imageWidth):
+        for j in range(imageHeight):
+            pixelColors = inputImage.getpixel((i, j))
+            copyImageOutput.putpixel((i, j), pixelColors)
+
+    copyImageOutput.save("/Users/notion/Desktop/CS110/project2/copy.png")
+
+def imageFlipper(inputImage, imageWidth, imageHeight):
+
+    imageWidth, imageHeight = inputImage.size
+    imageFlipped = Image.new(inputImage.mode, (imageWidth, imageHeight))
+
+    for x in range(imageWidth):
+        for y in range(imageHeight):
+            imageFlipped.putpixel((x, y), inputImage.getpixel((imageWidth - x - 1, y)))
+
+    imageFlipped.save("/Users/notion/Desktop/CS110/project2/flipped_image.png")
+
+
+main()
