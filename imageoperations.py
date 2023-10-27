96be3e12c8bc4611a592ab1911cca4242909407e
JP <2220635+jorgelightning@users.noreply.github.com>
2023-10-27 14:15:35 -0700

    Add files via upload

--- imageoperations.py
+++ imageoperations.py
@@ -1,4 +1,7 @@
+##
+
 from PIL import Image
+import math
 
 def main():
     imageName = 'usfca_logo.png'
@@ -9,13 +12,13 @@ def main():
 #Display menu and input validation, user will be repeated asked if input is incorrect 
     while True:
         try:
-            userAction = int(input('\nWelcome to image Operation program!\n\nWhat action would you like to take?\n(0)copy image (1)flip image (2)find pattern (3)make grey scale: '))
-            if not (0 <= userAction <= 3):
-                raise ValueError('Invalid number selection. Please enter number 0-3')
+            userAction = int(input('\nWelcome to image Operation program!\n\nWhat action would you like to take?\n(0)copy image (1)flip image (2)find pattern (3)make grey scale \n (4)rotate image (5) swapping corners (6)blurring image: '))
+            if not (0 <= userAction <= 6):
+                raise ValueError('Invalid number selection. Please enter number 0-4')
             break
         except ValueError:
-            print('\n\n### Please enter a valid integer from the selection (0-3). TRY AGAIN... ##\n\n')
-
+            print('\n\n### Please enter a valid integer from the selection (0-4). TRY AGAIN... ##\n\n')
+    
     imageWidth, imageHeight = inputImage.size
 
 #integers action selection
@@ -30,15 +33,21 @@ def main():
     
     if(userAction == 3):
         makeGrayscale(inputImage)
+    if(userAction == 4):
+        rotate(inputImage)
+    if(userAction == 5):
+        swapCorners(inputImage,imageWidth,imageHeight)
+    if(userAction == 6):
+        blur(inputImage,imageWidth,imageHeight)
 
 #Creates a copy of an image given the image variable, its width, and height
 def copyImage(inputImage, imageWidth, imageHeight):
     copyImageOutput = Image.new('RGB', (imageWidth, imageHeight), 'white')
 
-    for i in range(imageWidth):
-        for j in range(imageHeight):
-            pixelColors = inputImage.getpixel((i, j))
-            copyImageOutput.putpixel((i, j), pixelColors)
+    for x in range(imageWidth):
+        for y in range(imageHeight):
+            pixelColors = inputImage.getpixel((x, y))
+            copyImageOutput.putpixel((x, y), pixelColors)
 
     copyImageOutput.save("/Users/notion/Desktop/CS110/project2/copy.png")
 
@@ -85,12 +94,81 @@ def makeGrayscale(inputImage):
 
     grayscalePixelData = []
     for pixel in imagePixel:
-        grayscalePixel = (pixel[0] + pixel[1] + pixel[2]) // 3
-        #grayscalePixel = (int(pixel[0]*0.30) + int(pixel[1]*0.59) + int(pixel[2]*0.11))
+        #grayscalePixel = (pixel[0] + pixel[1] + pixel[2]) // 3
+        grayscalePixel = (int(pixel[0]*0.30) + int(pixel[1]*0.59) + int(pixel[2]*0.11))
         grayscalePixelData.append((grayscalePixel,grayscalePixel,grayscalePixel))
     
     inputImage.putdata(grayscalePixelData)
     inputImage.save("/Users/notion/Desktop/CS110/project2/grayscale_image.png")
 
+def rotate(inputImage):
 
-main()
+    rotatedImage = Image.new(inputImage.mode, (inputImage.height, inputImage.width))
+
+    for y in range(inputImage.height):
+        for x in range(inputImage.width):
+            pixelColors = inputImage.getpixel((x, y))
+            rotatedImage.putpixel((y, -x), pixelColors)
+    
+    rotatedImage.save("/Users/notion/Desktop/CS110/project2/rotated.png")
+
+def swapCorners(inputImage, imageWidth,imageHeight):
+
+    halfWidth = imageWidth // 2
+    halfHeight = imageHeight // 2
+
+    subsquare1 = inputImage.crop((0, 0, halfWidth, halfHeight))
+    subsquare2 = inputImage.crop((halfWidth, 0, imageWidth, halfHeight))
+    subsquare3 = inputImage.crop((0, halfHeight, halfWidth, imageHeight))
+    subsquare4 = inputImage.crop((halfWidth, halfHeight, imageWidth, imageHeight))
+
+    subsquares = [subsquare1,subsquare2,subsquare3,subsquare4]
+
+    cornerImage = Image.new('RGB', inputImage.size)
+    cornerImage.paste(subsquares[3], (0, 0))
+    cornerImage.paste(subsquares[2], (halfWidth, 0))
+    cornerImage.paste(subsquares[1], (0, halfHeight))
+    cornerImage.paste(subsquares[0], (halfWidth, halfHeight))
+    
+    cornerImage.save("/Users/notion/Desktop/CS110/project2/cornerswap.png")
+
+def blur(inputImage,imageWidth,imageHeight,):
+  
+  blurImage = Image.new('RGB', inputImage.size)
+
+  # Iterate over each pixel in the image.
+  for y in range(imageHeight):
+    for x in range(imageWidth):
+      
+      red_sum = 0
+      green_sum = 0
+      blue_sum = 0
+
+      for i in range(-1, 2):
+        for j in range(-1, 2):
+          
+          if x + i < 0 or x + i >= imageWidth or y + j < 0 or y + j >= imageHeight:
+            continue
+
+          red, green, blue = inputImage.getpixel((x + i, y + j))
+
+          red_sum += red
+          green_sum += green
+          blue_sum += blue
+
+      average_red = red_sum // 9
+      average_green = green_sum // 9
+      average_blue = blue_sum // 9
+
+      blurImage.putpixel((x, y), (average_red, average_green, average_blue))
+
+      blurImage.save("/Users/notion/Desktop/CS110/project2/blurred.png")
+
+def sharpenImage():
+    print('sharpening image')
+
+def edgedetection():
+    print('finding edge detection')
+
+
+main()  
