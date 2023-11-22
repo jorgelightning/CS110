04bc9d83b105a93dfb0be30719772ac89e8f2721
JP <2220635+jorgelightning@users.noreply.github.com>
2023-11-21 22:53:29 -0800

    Add files via upload

--- recursion_practice.py
+++ recursion_practice.py
@@ -5,8 +5,7 @@ def print_upto_n(num):
     else:
         print(num)
         print_upto_n(num-1)
-print('#1 Recursive Printing - print_upto_n(num)')
-print_upto_n(5)
+
 
 #2 Recursive Multiplication - rec_mul(x, y) 
 def rec_mul(x, y):
@@ -14,16 +13,14 @@ def rec_mul(x, y):
         return 0
     else:
         return x + rec_mul(x, y - 1)
-print('#2 Recursive Multiplication - rec_mul(x, y) ')
-print(rec_mul(2,2))
+
 
 #3 Recursive Lines - rec_n_asterisks (n)
 def rec_n_asterisks(n):
     if n > 0:
         print('*'*n)
         rec_n_asterisks(n-1)
-print('#3 Recursive Lines - rec_n_asterisks (n)')
-rec_n_asterisks(3)
+
 
 #4 Largest List Item - largest_in_list(input_list)
 def largest_in_list(input_list):
@@ -36,9 +33,7 @@ def largest_in_list(input_list):
             return input_list[-1]
         else:
             return current_max
-print('#4 Largest List Item - largest_in_list(input_list)')
-input_list = [1,2,3,4,5]
-print(largest_in_list(input_list))
+
 
 #5 Recursive List Sum - rec_list_sum(input_list)
 def rec_list_sum(input_list):
@@ -46,10 +41,7 @@ def rec_list_sum(input_list):
         return 0
     else:
         return input_list[0] + rec_list_sum(input_list[1:])
-input_list = [100, 200, 300, 400, 500]
-print('#5 Recursive List Sum - rec_list_sum(input_list)')
-print(input_list)
-print(rec_list_sum(input_list))
+
 
 #6 Sum of Numbers - rec_sum_to_n(num) 
 def sum_up_to_n(n):
@@ -57,18 +49,51 @@ def sum_up_to_n(n):
         return 1
     else:
         return n + sum_up_to_n(n - 1)
-print('#6 Sum of Numbers - rec_sum_to_n(num) ')
-print(sum_up_to_n(2))
+
 
 
 #7 Recursive Power Method rec_power(num, exponent)
 def rec_power(num, exponent):
-  if exponent == 0:
-    return 1
-  else:
-    return num * rec_power(num, exponent - 1)
+    if exponent == 0:
+        return 1
+    else:
+        return num * rec_power(num, exponent - 1)
 
-num = 2
-exponent = 4
-print('#7 Recursive Power Method rec_power(num, exponent)')
-print(rec_power(num, exponent))
+
+print('(1) Recursive Printing - print_upto_n(num)')
+print('(2) Recursive Multiplication - rec_mul(x, y) ')
+print('(3) Recursive Lines - rec_n_asterisks (n)')
+print('(4) Largest List Item - largest_in_list(input_list)')
+print('(5) Recursive List Sum - rec_list_sum(input_list)')
+print('(6) Sum of Numbers - rec_sum_to_n(num)')
+print('(7) Recursive Power Method rec_power(num, exponent)')
+userChoice = int(input('Please enter a positive interger of one of the menu item would you like to do?\n'))
+
+
+if userChoice == 1:
+    print('#1 Recursive Printing - print_upto_n(num)')
+    print_upto_n(5)
+if userChoice == 2:
+    print('#2 Recursive Multiplication - rec_mul(x, y) ')
+    print(rec_mul(2,2))
+if userChoice == 3:
+    print('#3 Recursive Lines - rec_n_asterisks (n)')
+    rec_n_asterisks(3)
+if userChoice == 4:
+    print('#4 Largest List Item - largest_in_list(input_list)')
+    input_list = [1,2,3,4,5]
+    print(largest_in_list(input_list))
+if userChoice == 5:
+    input_list = [100, 200, 300, 400, 500]
+    print('#5 Recursive List Sum - rec_list_sum(input_list)')
+    print(input_list)
+    print(rec_list_sum(input_list))
+if userChoice == 6:
+    print('#6 Sum of Numbers - rec_sum_to_n(num) ')
+    print(sum_up_to_n(2))
+if userChoice == 7:
+    num = 2
+    exponent = 4
+    print('#7 Recursive Power Method rec_power(num, exponent)')
+    print(rec_power(num, exponent))
+    
